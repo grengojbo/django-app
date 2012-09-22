e2929deb3571db86f7b09731386da4071a6ec622
Oleg Dolya <oleg.dolya@gmail.com>
2012-09-23 02:02:14 +0300

    Added django-flatpages-plus

--- DjangoApp/settings/base.py
+++ DjangoApp/settings/base.py
@@ -68,7 +68,7 @@ INSTALLED_APPS = [
     #'debug_toolbar_user_panel',
     #'memcache_toolbar',
     'intellipages',
-
+    'flatpages_plus',
     # Database migrations
     'south',
 
@@ -151,6 +151,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'session_csrf.CsrfMiddleware',  # Must be after auth middleware.
     'django.contrib.messages.middleware.MessageMiddleware',
+    'flatpages_plus.middleware.FlatpageFallbackMiddleware',
     #'commonware.middleware.FrameOptionsHeader',
     #'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
