41fbf3c24729c8cc1160ca2454e003a260e4ac54
Oleg Dolya <oleg.dolya@gmail.com>
2012-10-13 18:37:41 +0300

    Fix bug locale switcher

--- DjangoApp/settings/base.py
+++ DjangoApp/settings/base.py
@@ -64,7 +64,7 @@ INSTALLED_APPS = [
     'commonware.response.cookies',
     'djcelery',
     'gunicorn',
-    'modeltranslation',
+    #'modeltranslation',
     #'django_nose',
     #'session_csrf',
     #'debug_toolbar',
