5f01216376a02639a611be5213e548946cf0b319
Oleg Dolya <oleg.dolya@gmail.com>
2012-11-02 23:33:28 +0200

    fix bug

--- DjangoApp/settings/base.py
+++ DjangoApp/settings/base.py
@@ -168,17 +168,9 @@ USE_TZ = True
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-)
-
-if 'dajaxice' in INSTALLED_APPS:
-    STATICFILES_FINDERS = STATICFILES_FINDERS + (
     'dajaxice.finders.DajaxiceFinder',
-    )
-
-if 'compressor' in INSTALLED_APPS:
-    STATICFILES_FINDERS = STATICFILES_FINDERS + ( 
     'compressor.finders.CompressorFinder',
-    )
+)
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
--- DjangoApp/urls.py
+++ DjangoApp/urls.py
@@ -18,8 +18,7 @@ from django.contrib import admin
 from filebrowser.sites import site
 
 admin.autodiscover()
-if 'dajaxice' in settings.INSTALLED_APPS:
-    dajaxice_autodiscover()
+dajaxice_autodiscover()
 
 # django-session-csrf monkeypatcher
 #import session_csrf
@@ -56,6 +55,9 @@ urlpatterns += patterns('',
     #url(r'^$', direct_to_template, {'template': 'static/promo.html'}, name='promo'),
     (r'^i18n/', include('django.conf.urls.i18n')),
     (r'^jsi18n/$', 'django.views.i18n.javascript_catalog', {'packages': ('fiber',),}),
+    (dajaxice_config.dajaxice_url, include('dajaxice.urls')),
+    #url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
+    #(r'^accounts/', include('registration.backends.default.urls')),
 )
 
 if 'fiber' in settings.INSTALLED_APPS:
@@ -70,11 +72,6 @@ else:
 if 'rosetta' in settings.INSTALLED_APPS:
     urlpatterns += patterns('',
         url(r'^rosetta/', include('rosetta.urls')),
-    )
-
-if 'dajaxice' in settings.INSTALLED_APPS:
-    urlpatterns += patterns('',
-    (dajaxice_config.dajaxice_url, include('dajaxice.urls')),
     )
 
 #https://docs.djangoproject.com/en/dev/topics/i18n/translation/#url-internationalization
