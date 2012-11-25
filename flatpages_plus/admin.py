8243f7a392ecb7b0b2107c8d5b2dab8aa7782bbe
Oleg Dolya <oleg.dolya@gmail.com>
2012-11-25 18:17:34 +0200

    modify flatpage admin

--- flatpages_plus/admin.py
+++ flatpages_plus/admin.py
@@ -7,35 +7,36 @@ from flatpages_plus.forms import FlatpageForm
 from flatpages_plus.models import FlatPage
 
 
-try:
-    from grappellifit.admin import TranslationAdmin, TranslationStackedInline
-    ModelAdmin = TranslationAdmin
-    StackedInlineAdmin = TranslationStackedInline
-except:
-    try:
-        from modeltranslation.admin import TranslationAdmin, TranslationStackedInline
-        ModelAdmin = TranslationAdmin
-        StackedInlineAdmin = TranslationStackedInline
-    except:
-        ModelAdmin = admin.ModelAdmin
-        StackedInlineAdmin = admin.StackedInline
+#try:
+#    from grappellifit.admin import TranslationAdmin, TranslationStackedInline
+#    ModelAdmin = TranslationAdmin
+#    StackedInlineAdmin = TranslationStackedInline
+#except:
+#    try:
+#        from modeltranslation.admin import TranslationAdmin, TranslationStackedInline
+#        ModelAdmin = TranslationAdmin
+#        StackedInlineAdmin = TranslationStackedInline
+#    except:
+#        ModelAdmin = admin.ModelAdmin
+#        StackedInlineAdmin = admin.StackedInline
+
+ModelAdmin = admin.ModelAdmin
+StackedInlineAdmin = admin.StackedInline
 
 class FlatPageAdmin(ModelAdmin):
     #form = FlatpageForm
     fieldsets = (
-    (_('Title'), {
-              'fields': ('title',)
-                          }),
+        (_('Title'), {
+              'fields': ('title','url',)
+        }),
         (None, {'fields': (
-          'url',
-            'name',
-            'owner',
-            'status',
+            ('owner','status',),
             'content',
             'tags',
+            'name',
         )}),
         (_('Advanced options'), {
-            'classes': ('collapse',), 
+            'classes': ('grp-collapse grp-closed',),
             'fields': (
                 'sites',
                 'enable_comments', 
@@ -48,7 +49,13 @@ class FlatPageAdmin(ModelAdmin):
     #prepopulated_fields = {'url': ('title',)}
     list_display = ('url', 'title', 'name', 'status', 'owner', 'views', 'modified', 'created')
     list_filter = ('status', 'sites', 'enable_comments', 'registration_required',)
+    prepopulated_fields = {'url': ('title',)}
     search_fields = ('url', 'title', 'name', 'owner',)
+    class Media:
+        js = [
+            "{0}grappelli/tinymce/jscripts/tiny_mce/tiny_mce.js".format(settings.STATIC_URL),
+            "{0}js/tinymce_setup.js".format(settings.STATIC_URL),
+        ]
     #"{0}modeltranslation/js/force_jquery.js".format(settings.STATIC_URL),
     #"{0}modeltranslation/js/tabbed_translation_fields.js".format(settings.STATIC_URL),
     #class Media:
